cc645d8dd5737f985c2b8660798886563842bc67
Anton Zhuravsky <anton@lunalabs.uk>
2020-08-24 15:41:44 +0300

    Changing base class for SchemaCreation

--- lib/active_record/connection_adapters/clickhouse/schema_creation.rb
+++ lib/active_record/connection_adapters/clickhouse/schema_creation.rb
@@ -3,7 +3,7 @@
 module ActiveRecord
   module ConnectionAdapters
     module Clickhouse
-      class SchemaCreation < AbstractAdapter::SchemaCreation# :nodoc:
+      class SchemaCreation < SchemaCreation# :nodoc:
 
         def visit_AddColumnDefinition(o)
           +"ADD COLUMN #{accept(o.column)}"
